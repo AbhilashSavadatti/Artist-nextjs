03e1c10f65f26aadb8577956d1b656a139da347d
001101010 <abhilash18082002@gmail.com>
2024-07-23 14:33:04 +0530

    env added

---
+++ src/app/celebrity/page.tsx
@@ -0,0 +1,151 @@
+import React from 'react'
+
+function page() {
+  return (
+    <>
+    
+    <section
+  id="Artists"
+  className="pr-20 pl-20 w-fit mx-auto grid grid-cols-1 lg:grid-cols-4 md:grid-cols-2 justify-items-center justify-center gap-y-20 gap-x-14 mt-10 mb-5"
+>
+  <div className="w-72 bg-white shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl">
+    <a href="Inquiry.html">
+      <img
+        src="images/03-amitabh-bacchan-740x740.jpg"
+        alt="Product"
+        className="h-80 w-72 object-cover rounded-t-xl"
+      />
+      <div className="mx-auto py-10">
+        <div className="flex justify-center items-center">
+          <p className="text-lg font-bold text-black capitalize">
+            Amitabh Bachchan
+          </p>
+          <span className="text-gray-400 ml-2 uppercase text-xs">[Male]</span>
+        </div>
+      </div>
+    </a>
+  </div>
+  <div className="w-72 bg-white shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl">
+    <a href="#">
+      <img
+        src="images/03-shahrukh-khan-740x740.png"
+        alt="Product"
+        className="h-80 w-72 object-cover rounded-t-xl"
+      />
+      <div className="mx-auto py-10">
+        <div className="flex justify-center items-center">
+          <p className="text-lg font-bold text-black capitalize">
+            Shahrukh Khan
+          </p>
+          <span className="text-gray-400 ml-2 uppercase text-xs">[Male]</span>
+        </div>
+      </div>
+    </a>
+  </div>
+  <div className="w-72 bg-white shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl">
+    <a href="#">
+      <img
+        src="images/03-salman-khan-740x740.png"
+        alt="Product"
+        className="h-80 w-72 object-cover rounded-t-xl"
+      />
+      <div className="mx-auto py-10">
+        <div className="flex justify-center items-center">
+          <p className="text-lg font-bold text-black capitalize">Salman Khan</p>
+          <span className="text-gray-400 ml-2 uppercase text-xs">[Male]</span>
+        </div>
+      </div>
+    </a>
+  </div>
+  <div className="w-72 bg-white shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl">
+    <a href="#">
+      <img
+        src="images/03-aishwarya-rai-740x740.png"
+        alt="Product"
+        className="h-80 w-72 object-cover rounded-t-xl"
+      />
+      <div className="mx-auto py-10">
+        <div className="flex justify-center items-center">
+          <p className="text-lg font-bold text-black capitalize">
+            Aishwarya Rai Bachchan
+          </p>
+          <span className="text-gray-400 ml-2 uppercase text-xs">[Female]</span>
+        </div>
+      </div>
+    </a>
+  </div>
+  <div className="w-72 bg-white shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl">
+    <a href="#">
+      <img
+        src="images/03-amir-khan-740x740.png"
+        alt="Product"
+        className="h-80 w-72 object-cover rounded-t-xl"
+      />
+      <div className="mx-auto py-10">
+        <div className="flex justify-center items-center">
+          <p className="text-lg font-bold text-black capitalize">Aamir Khan</p>
+          <span className="text-gray-400 ml-2 uppercase text-xs">[Male]</span>
+        </div>
+      </div>
+    </a>
+  </div>
+  <div className="w-72 bg-white shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl">
+    <a href="#">
+      <img
+        src="images/03-akshay-kumar-740x740.jpg"
+        alt="Product"
+        className="h-80 w-72 object-cover rounded-t-xl"
+      />
+      <div className="mx-auto py-10">
+        <div className="flex justify-center items-center">
+          <p className="text-lg font-bold text-black capitalize">
+            Akshay Kumar
+          </p>
+          <span className="text-gray-400 ml-2 uppercase text-xs">[Male]</span>
+        </div>
+      </div>
+    </a>
+  </div>
+  <div className="w-72 bg-white shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl">
+    <a href="#">
+      <img
+        src="images/03-hrithik-roshan-740x740.jpg"
+        alt="Product"
+        className="h-80 w-72 object-cover rounded-t-xl"
+      />
+      <div className="mx-auto py-10">
+        <div className="flex justify-center items-center">
+          <p className="text-lg font-bold text-black capitalize">
+            Hrithik Roshan
+          </p>
+          <span className="text-gray-400 ml-2 uppercase text-xs">[Male]</span>
+        </div>
+      </div>
+    </a>
+  </div>
+  <div className="w-72 bg-white shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl">
+    <a href="#">
+      <img
+        src="images/03-shahid-kapoor-740x740.jpg"
+        alt="Product"
+        className="h-80 w-72 object-cover rounded-t-xl"
+      />
+      <div className="mx-auto py-10">
+        <div className="flex justify-center items-center">
+          <p className="text-lg font-bold text-black capitalize">
+            Shahid Kapoor
+          </p>
+          <span className="text-gray-400 ml-2 uppercase text-xs">[Male]</span>
+        </div>
+      </div>
+    </a>
+  </div>
+</section>
+
+    
+    
+    </>
+  )
+}
+
+export default page
