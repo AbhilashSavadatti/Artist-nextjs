c266fc4aa1fb9c15e7e4ff7591746922c0cb28e8
001101010 <abhilash18082002@gmail.com>
2024-07-23 16:05:59 +0530

    wrapper

--- src/components/HeroSectionWrapper.tsx
+++ src/components/HeroSectionWrapper.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { usePathname } from "next/navigation";
-import HeroSection from "@/components/herosection"; // Correct path
+import HeroSection from "@/components/herosection"; 
 
 export default function HeroSectionWrapper() {
   const pathname = usePathname();
