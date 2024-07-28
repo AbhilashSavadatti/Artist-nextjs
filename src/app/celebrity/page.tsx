8872ba0528bc0307642cc99ecaa304fe60dd4e93
001101010 <abhilash18082002@gmail.com>
2024-07-28 18:45:17 +0530

    added inquiry form

--- src/app/celebrity/page.tsx
+++ src/app/celebrity/page.tsx
@@ -106,7 +106,7 @@ function Page() {
                 <div className="flex items-center mx-auto justify-center mt-2">
                   <a
                     className="inline-block rounded bg-indigo-600 px-4 py-2 text-sm font-medium text-white transition-transform transform hover:scale-110 hover:shadow-xl focus:outline-none focus:ring active:bg-indigo-500"
-                    href="#"
+                    href="inquiry"
                   >
                     Book
                   </a>
